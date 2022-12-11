e99a6868facf34d9f3b3055b04da8c925d5c7da5
2518508m <2518508M@student.gla.ac.uk>
2022-12-11 16:37:24 +0000

    Update

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+import { NextPage } from 'next';
+import React from 'react';
+
+
+let Index: NextPage = () => {
+  return (
+    <div>
+      <h1 className="underline text-3xl">This is the Discover Page!</h1>
+      </div>
+  );
+};
+
+export default Index;
